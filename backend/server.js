76f87a96050a1317e9a53b5524539f0f01775737
vahghar <raghav10g@gmail.com>
2025-12-18 05:48:41 +0530

    added-env-data

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
